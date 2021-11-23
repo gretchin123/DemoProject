323e36f20f228e4475cf01dc12f895a37eda19b6
Gretchin <gretchin.ogao@deepdata.com>
2021-11-23 22:10:38 +0800

    Updated the class Smoke_Demo.java by listing the variables.

--- src/test/java/SmokeTesting/Smoke_Demo.java
+++ src/test/java/SmokeTesting/Smoke_Demo.java
@@ -1,4 +1,19 @@
 package SmokeTesting;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+
 public class Smoke_Demo {
+
+    WebDriver driver;
+    XSSFWorkbook workbook;
+    XSSFSheet sheet;
+    XSSFCell cell;
+    DataFormatter dataFormatter = new DataFormatter();
+    String AutomationResultPassed = "Passed";
+    String AutomationResultFailed = "Failed";
+
 }
